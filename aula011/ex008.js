a798d0f57b9a5ec16fc9bc851371195f2febd332
ViniciusNitta <yoshikivinicius2003@gmail.com>
2023-05-30 16:03:25 -0300

    ex 008 009 010 e 011

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 56
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida!!`)
+}
+console.log(`Dirija sempre com o cinto de segurança!`)
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Nascendo no ${pais}, você é:`)
+if (pais == 'Brasil'){
+    console.log(`Brasileiro`)
+}else {
+    console.log(`Estrangeiro`)
+}
